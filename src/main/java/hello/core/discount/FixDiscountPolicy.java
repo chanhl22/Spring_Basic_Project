84a2c18202db18c3dab68fec9c9a36c88dfcf275
chanhl22 <chanhlee@student.42seoul.kr>
2021-09-24 16:45:05 +0900

    주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000; //무조건 1,000원 할인해주니까
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
