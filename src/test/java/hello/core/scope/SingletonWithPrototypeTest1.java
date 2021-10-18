45775feff62dfd1fa313e0f9cf55f2047775a723
chanhl22 <chanhlee@student.42seoul.kr>
2021-10-18 14:24:09 +0900

    Provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,6 +3,7 @@ package hello.core.scope;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -39,26 +41,42 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean{
-//        private final PrototypeBean prototypeBean; //생성시점에 주입
-
         @Autowired
-        ApplicationContext applicationContext;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+
+//        싱글톤 빈 안에 프로토타입 빈
+//        private final PrototypeBean prototypeBean; //생성시점에 주입
+//
 //        @Autowired
 //        public ClientBean(PrototypeBean prototypeBean) {
 //            this.prototypeBean = prototypeBean;
 //        }
+//
+//        public int logic() {
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
 
-        public int logic() {
-            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
-            prototypeBean.addCount();
-            return prototypeBean.getCount();
-        }
+//        스프링 컨테이너에 요청
+//        @Autowired
+//        ApplicationContext applicationContext;
+//
+//        public int logic() {
+//            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
     }
 
     @Scope("prototype")
